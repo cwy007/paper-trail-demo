f19b0fefce2a6ac568fe9b59f075a8747d267e71
cwy007 <chanweiyan007@gmail.com>
2017-09-10 15:42:00 +0800

    add versions controller and revert routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
 
+  post "versions/:id/revert" => "versions#revert", :as => "revert_version"
+
   root 'articles#index'
 end
